e91761e1446072ff0b4c3c499eb28ea2a8c8f7a5
sinryuji <siler2474@gmail.com>
2025-03-05 18:58:07 +0900

    store cache with redis

--- src/main/java/com/example/redis/domain/Store.java
+++ src/main/java/com/example/redis/domain/Store.java
@@ -1,22 +1,20 @@
-package com.example.redis;
+package com.example.redis.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
 
+
 @Getter
 @Entity
-@Table(name = "orders")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemOrder {
+public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;
-    private Integer count;
+    private String name;
+    @Setter
+    private String category;
 }
